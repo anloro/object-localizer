bd2c8c19abf66b1ee4fc537516db3c3c4d5af050
anloro <angel.lr97@gmail.com>
2022-12-01 18:46:08 +0100

    white filter refactor

--- src/object_detector.py
+++ src/object_detector.py
@@ -19,23 +19,7 @@ class ObjectDetector:
             - frame_result (np.array): Preprocessed image.
         """
 
-        # Convert BGR to HSV
-        frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        # Threshold of white in HSV space
-        pure_white_hsv = np.array([0, 0, 210])
-        pale_gray_hsv = np.array([255, 15, 255])
-
-        # Create and use the mask
-        mask = cv.inRange(frame_hsv, pure_white_hsv, pale_gray_hsv)
-        # Get rid off noise
-        kernel = np.ones((5, 5), np.uint8)
-        mask = cv.erode(mask, kernel, iterations=2)
-        mask = cv.dilate(mask, kernel, iterations=2)
-
-        # Apply the mask
-        frame_masked_hsv = cv.bitwise_and(frame_hsv, frame_hsv, mask=mask)
-        # Return to BGR format
-        frame_result = cv.cvtColor(frame_masked_hsv, cv.COLOR_HSV2BGR)
+        frame_result = self.white_filter(frame)
         cv.imshow("frame_result", frame_result)
         cv.waitKey(0)
 
@@ -78,9 +62,37 @@ class ObjectDetector:
 
         return frame_boxed
 
+    def white_filter(self, frame):
+        """Creates a mask to filter the image using a white threshold.
+
+        input:
+            - frame: Input image
+        output:
+            - frame_result: Filtered image.
+        """
+        # Convert BGR to HSV
+        frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        # Threshold of white in HSV space
+        pure_white_hsv = np.array([0, 0, 210])
+        pale_gray_hsv = np.array([255, 15, 255])
+
+        # Create and use the mask
+        mask = cv.inRange(frame_hsv, pure_white_hsv, pale_gray_hsv)
+        # Get rid off noise
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv.erode(mask, kernel, iterations=2)
+        mask = cv.dilate(mask, kernel, iterations=2)
+
+        # Apply the mask
+        frame_masked_hsv = cv.bitwise_and(frame_hsv, frame_hsv, mask=mask)
+        # Return to BGR format
+        frame_result = cv.cvtColor(frame_masked_hsv, cv.COLOR_HSV2BGR)
+
+        return frame_result
+
     def approximate_squares(self, contours):
         """
-        Computes the arc length and approximates a polygon. 
+        Computes the arc length and approximates a polygon.
         If the approximated polygon has 4 vertexes it saves the
         results and it returns it.
         """
@@ -90,5 +102,5 @@ class ObjectDetector:
             approx = cv.approxPolyDP(contour, 0.04 * arc_length, True)
             if len(approx) == 4:
                 squares.append(approx)
-        
+
         return squares
